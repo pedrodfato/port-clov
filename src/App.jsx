a49f26a65f57f1747c32925c9297bdbd017591a9
pedro <pedro.casstrof@gmail.com>
2026-01-15 02:50:32 -0300

    pagina 404 adicionada + novo favicon/titulo

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import { React } from 'react'
 import './App.css'
+import FloatingTexts from './components/floatinsElements'
 
 function App() {
 
   return (
-    <h1>Oi</h1>
+    <section className="h-screen w-screen bg-black flex flex-col justify-center items-center relative overflow-hidden">
+       <FloatingTexts />
+      <div className="flex justify-center items-center flex-col max-w-[1100px] py-[60px] text-center">
+        <h1 className=" text-[140px] font-bold neon-text">4<span className="trevor"></span>4</h1>
+        <p className="font-mono absolute top-9 left-[42%]">Website is under construction</p>
+      </div>
+      <div className="green-glow"></div>
+    </section>
   )
 }
 
